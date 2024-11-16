df47cef1b213a35a32bf042fe57443e4aa9f1e66
Werdy59 <142653732+Werdy59@users.noreply.github.com>
2024-11-16 18:47:31 +0100

    help command created

---
+++ index.js
@@ -0,0 +1,11 @@
+import {} from 'dotenv/config'
+
+import { Client, Collection, GatewayIntentBits } from 'discord.js';
+import fs from 'fs';
+import path from 'path';
+import { exec } from 'child_process';
+import { fileURLToPath } from 'url';
+
+
+
+
